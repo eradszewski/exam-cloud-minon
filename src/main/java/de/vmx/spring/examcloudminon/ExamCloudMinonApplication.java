332af8c6605feae7755e0eb40034396cc1a14624
er <eradszewski@gmail.com>
2018-03-27 23:37:19 +0200

    first commit

---
+++ src/main/java/de/vmx/spring/examcloudminon/ExamCloudMinonApplication.java
@@ -0,0 +1,12 @@
+package de.vmx.spring.examcloudminon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCloudMinonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamCloudMinonApplication.class, args);
+	}
+}
---
+++ src/test/java/de/vmx/spring/examcloudminon/ExamCloudMinonApplicationTests.java
@@ -0,0 +1,16 @@
+package de.vmx.spring.examcloudminon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamCloudMinonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
